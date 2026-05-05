2052e4a6a457ec736270f270e4ed457775996ff2
Mohammad Anas <ansarianas53665@gmail.com>
2026-05-05 21:43:56 +0530

    change in prisma

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -2,4 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+const maskedUrl = process.env.DATABASE_URL?.replace(/:([^@]+)@/, ':****@');
+console.log(`Prisma initialized with URL: ${maskedUrl}`);
+
 module.exports = prisma;
