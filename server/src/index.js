11e0f0619043f469c14c3d878b67ecc4bbcb19ff
Mohammad Anas <ansarianas53665@gmail.com>
2026-05-06 14:56:52 +0530

    fix cors final

--- server/src/index.js
+++ server/src/index.js
@@ -12,23 +12,14 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-// CORS
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://team-task-manager-production-d387.up.railway.app"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    "http://localhost:5173",
+    "https://team-task-manager-production-d387.up.railway.app"
+  ],
   credentials: true,
 }));
-// Middleware
+
 app.use(express.json());
 app.use(cookieParser());
 
